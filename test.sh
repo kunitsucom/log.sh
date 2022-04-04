d32ff05d2b46afce94ca5e07a3043cd7b57078d4
newtstat <29125616+newtstat@users.noreply.github.com>
2022-04-04 21:47:14 +0900

    fix for empty json message value

--- test.sh
+++ test.sh
@@ -11,6 +11,7 @@ export TZ=UTC REC_SEVERITY=0 REC_TIMESTAMP_KEY=timestamp REC_SEVERITY_KEY=severi
 #
 
 TestRecDefaultJSON_001() {
+  unset REC_SEVERITY
   export REC_SEVERITY=0
   if ! RecDefaultJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"DEFAULT","caller":"./test.sh","message":"\\t\\nA\\t\\nB\\t\\nC\\t","test":"\\t\\nA\\t\\nB\\t\\nC\\t"}$'; then
     echo "FAIL: TestRecDefaultJSON_001"
@@ -18,9 +19,9 @@ TestRecDefaultJSON_001() {
   fi
   echo "PASS: TestRecDefaultJSON_001"
 } && TestRecDefaultJSON_001
-unset REC_SEVERITY
 
 TestRecDefaultJSON_002() {
+  unset REC_SEVERITY
   export REC_SEVERITY=1
   if [ "$(RecDefaultJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1)" ]; then
     echo "FAIL: TestRecDefaultJSON_002"
@@ -28,13 +29,13 @@ TestRecDefaultJSON_002() {
   fi
   echo "PASS: TestRecDefaultJSON_002"
 } && TestRecDefaultJSON_002
-unset REC_SEVERITY
 
 #
 # RecDebugJSON
 #
 
 TestRecDebugJSON_001() {
+  unset REC_SEVERITY
   export REC_SEVERITY=100
   if ! RecDebugJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"DEBUG","caller":"./test.sh","message":"\\t\\nA\\t\\nB\\t\\nC\\t","test":"\\t\\nA\\t\\nB\\t\\nC\\t"}$'; then
     echo "FAIL: TestRecDebugJSON_001"
@@ -42,9 +43,9 @@ TestRecDebugJSON_001() {
   fi
   echo "PASS: TestRecDebugJSON_001"
 } && TestRecDebugJSON_001
-unset REC_SEVERITY
 
 TestRecDebugJSON_002() {
+  unset REC_SEVERITY
   export REC_SEVERITY=101
   if [ "$(RecDebugJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1)" ]; then
     echo "FAIL: TestRecDebugJSON_002"
@@ -52,13 +53,13 @@ TestRecDebugJSON_002() {
   fi
   echo "PASS: TestRecDebugJSON_002"
 } && TestRecDebugJSON_002
-unset REC_SEVERITY
 
 #
 # RecInfoJSON
 #
 
 TestRecInfoJSON_001() {
+  unset REC_SEVERITY
   export REC_SEVERITY=200
   if ! RecInfoJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"INFO","caller":"./test.sh","message":"\\t\\nA\\t\\nB\\t\\nC\\t","test":"\\t\\nA\\t\\nB\\t\\nC\\t"}$'; then
     echo "FAIL: TestRecInfoJSON_001"
@@ -66,9 +67,9 @@ TestRecInfoJSON_001() {
   fi
   echo "PASS: TestRecInfoJSON_001"
 } && TestRecInfoJSON_001
-unset REC_SEVERITY
 
 TestRecInfoJSON_002() {
+  unset REC_SEVERITY
   export REC_SEVERITY=201
   if [ "$(RecInfoJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1)" ]; then
     echo "FAIL: TestRecInfoJSON_002"
@@ -76,13 +77,13 @@ TestRecInfoJSON_002() {
   fi
   echo "PASS: TestRecInfoJSON_002"
 } && TestRecInfoJSON_002
-unset REC_SEVERITY
 
 #
 # RecNoticeJSON
 #
 
 TestRecNoticeJSON_001() {
+  unset REC_SEVERITY
   export REC_SEVERITY=300
   if ! RecNoticeJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"NOTICE","caller":"./test.sh","message":"\\t\\nA\\t\\nB\\t\\nC\\t","test":"\\t\\nA\\t\\nB\\t\\nC\\t"}$'; then
     echo "FAIL: TestRecNoticeJSON_001"
@@ -90,9 +91,9 @@ TestRecNoticeJSON_001() {
   fi
   echo "PASS: TestRecNoticeJSON_001"
 } && TestRecNoticeJSON_001
-unset REC_SEVERITY
 
 TestRecNoticeJSON_002() {
+  unset REC_SEVERITY
   export REC_SEVERITY=301
   if [ "$(RecNoticeJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1)" ]; then
     echo "FAIL: TestRecNoticeJSON_002"
@@ -100,13 +101,13 @@ TestRecNoticeJSON_002() {
   fi
   echo "PASS: TestRecNoticeJSON_002"
 } && TestRecNoticeJSON_002
-unset REC_SEVERITY
 
 #
 # RecWarningJSON
 #
 
 TestRecWarningJSON_001() {
+  unset REC_SEVERITY
   export REC_SEVERITY=400
   if ! RecWarningJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"WARNING","caller":"./test.sh","message":"\\t\\nA\\t\\nB\\t\\nC\\t","test":"\\t\\nA\\t\\nB\\t\\nC\\t"}$'; then
     echo "FAIL: TestRecWarningJSON_001"
@@ -114,9 +115,9 @@ TestRecWarningJSON_001() {
   fi
   echo "PASS: TestRecWarningJSON_001"
 } && TestRecWarningJSON_001
-unset REC_SEVERITY
 
 TestRecWarningJSON_002() {
+  unset REC_SEVERITY
   export REC_SEVERITY=401
   if [ "$(RecWarningJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1)" ]; then
     echo "FAIL: TestRecWarningJSON_002"
@@ -124,13 +125,13 @@ TestRecWarningJSON_002() {
   fi
   echo "PASS: TestRecWarningJSON_002"
 } && TestRecWarningJSON_002
-unset REC_SEVERITY
 
 #
 # RecErrorJSON
 #
 
 TestRecErrorJSON_001() {
+  unset REC_SEVERITY
   export REC_SEVERITY=500
   if ! RecErrorJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"ERROR","caller":"./test.sh","message":"\\t\\nA\\t\\nB\\t\\nC\\t","test":"\\t\\nA\\t\\nB\\t\\nC\\t"}$'; then
     echo "FAIL: TestRecErrorJSON_001"
@@ -138,9 +139,9 @@ TestRecErrorJSON_001() {
   fi
   echo "PASS: TestRecErrorJSON_001"
 } && TestRecErrorJSON_001
-unset REC_SEVERITY
 
 TestRecErrorJSON_002() {
+  unset REC_SEVERITY
   export REC_SEVERITY=501
   if [ "$(RecErrorJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1)" ]; then
     echo "FAIL: TestRecErrorJSON_002"
@@ -148,13 +149,13 @@ TestRecErrorJSON_002() {
   fi
   echo "PASS: TestRecErrorJSON_002"
 } && TestRecErrorJSON_002
-unset REC_SEVERITY
 
 #
 # RecCriticalJSON
 #
 
 TestRecCriticalJSON_001() {
+  unset REC_SEVERITY
   export REC_SEVERITY=600
   if ! RecCriticalJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"CRITICAL","caller":"./test.sh","message":"\\t\\nA\\t\\nB\\t\\nC\\t","test":"\\t\\nA\\t\\nB\\t\\nC\\t"}$'; then
     echo "FAIL: TestRecCriticalJSON_001"
@@ -162,9 +163,9 @@ TestRecCriticalJSON_001() {
   fi
   echo "PASS: TestRecCriticalJSON_001"
 } && TestRecCriticalJSON_001
-unset REC_SEVERITY
 
 TestRecCriticalJSON_002() {
+  unset REC_SEVERITY
   export REC_SEVERITY=601
   if [ "$(RecCriticalJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1)" ]; then
     echo "FAIL: TestRecCriticalJSON_002"
@@ -172,13 +173,13 @@ TestRecCriticalJSON_002() {
   fi
   echo "PASS: TestRecCriticalJSON_002"
 } && TestRecCriticalJSON_002
-unset REC_SEVERITY
 
 #
 # RecAlertJSON
 #
 
 TestRecAlertJSON_001() {
+  unset REC_SEVERITY
   export REC_SEVERITY=700
   if ! RecAlertJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"ALERT","caller":"./test.sh","message":"\\t\\nA\\t\\nB\\t\\nC\\t","test":"\\t\\nA\\t\\nB\\t\\nC\\t"}$'; then
     echo "FAIL: TestRecAlertJSON_001"
@@ -186,9 +187,9 @@ TestRecAlertJSON_001() {
   fi
   echo "PASS: TestRecAlertJSON_001"
 } && TestRecAlertJSON_001
-unset REC_SEVERITY
 
 TestRecAlertJSON_002() {
+  unset REC_SEVERITY
   export REC_SEVERITY=701
   if [ "$(RecAlertJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1)" ]; then
     echo "FAIL: TestRecAlertJSON_002"
@@ -196,13 +197,13 @@ TestRecAlertJSON_002() {
   fi
   echo "PASS: TestRecAlertJSON_002"
 } && TestRecAlertJSON_002
-unset REC_SEVERITY
 
 #
 # RecEmergencyJSON
 #
 
 TestRecEmergencyJSON_001() {
+  unset REC_SEVERITY
   export REC_SEVERITY=800
   if ! RecEmergencyJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"EMERGENCY","caller":"./test.sh","message":"\\t\\nA\\t\\nB\\t\\nC\\t","test":"\\t\\nA\\t\\nB\\t\\nC\\t"}$'; then
     echo "FAIL: TestRecEmergencyJSON_001"
@@ -210,9 +211,9 @@ TestRecEmergencyJSON_001() {
   fi
   echo "PASS: TestRecEmergencyJSON_001"
 } && TestRecEmergencyJSON_001
-unset REC_SEVERITY
 
 TestRecEmergencyJSON_002() {
+  unset REC_SEVERITY
   export REC_SEVERITY=801
   if [ "$(RecEmergencyJSON "$(printf "\t\nA\t\nB\t\nC\t\n")" "test" "$(printf "\t\nA\t\nB\t\nC\t\n")" 2>&1)" ]; then
     echo "FAIL: TestRecEmergencyJSON_002"
@@ -220,13 +221,13 @@ TestRecEmergencyJSON_002() {
   fi
   echo "PASS: TestRecEmergencyJSON_002"
 } && TestRecEmergencyJSON_002
-unset REC_SEVERITY
 
 #
 # RecExecJSON
 #
 
 TestRecExecJSON_001() {
+  unset REC_SEVERITY
   export REC_SEVERITY=100
   if ! RecExecJSON true 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"INFO","caller":"./test.sh","message":". true"}$'; then
     echo "FAIL: TestRecExecJSON_001"
@@ -234,9 +235,9 @@ TestRecExecJSON_001() {
   fi
   echo "PASS: TestRecExecJSON_001"
 } && TestRecExecJSON_001
-unset REC_SEVERITY
 
 TestRecExecJSON_002() {
+  unset REC_SEVERITY
   export REC_SEVERITY=201
   if [ "$(RecExecJSON true 2>&1)" ]; then
     echo "FAIL: TestRecExecJSON_002"
@@ -244,13 +245,13 @@ TestRecExecJSON_002() {
   fi
   echo "PASS: TestRecExecJSON_002"
 } && TestRecExecJSON_002
-unset REC_SEVERITY
 
 #
 # RecRunJSON
 #
 
 TestRecRunJSON_001() {
+  unset REC_SEVERITY
   export REC_SEVERITY=100
   if ! RecRunJSON date +%Y-%m-%d 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"INFO","caller":"./test.sh","message":". date .%Y-%m-%d","command":"date .%Y-%m-%d","stdout":"[0-9]+-[0-9]+-[0-9]+","stderr":"","return":"0"}$'; then
     echo "FAIL: TestRecRunJSON_001"
@@ -258,9 +259,9 @@ TestRecRunJSON_001() {
   fi
   echo "PASS: TestRecRunJSON_001"
 } && TestRecRunJSON_001
-unset REC_SEVERITY
 
 TestRecRunJSON_002() {
+  unset REC_SEVERITY
   export REC_SEVERITY=201
   if [ "$(RecRunJSON date +%Y-%m-%d 2>&1)" ]; then
     echo "FAIL: TestRecRunJSON_002"
@@ -268,4 +269,37 @@ TestRecRunJSON_002() {
   fi
   echo "PASS: TestRecRunJSON_002"
 } && TestRecRunJSON_002
-unset REC_SEVERITY
+
+#
+# common
+#
+
+TestCommonRecJSON_001() {
+  unset REC_SEVERITY
+  export REC_SEVERITY=0
+  if ! RecDefaultJSON 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"DEFAULT","caller":"./test.sh","message":""}$'; then
+    echo "FAIL: TestCommonRecJSON_001"
+    return 1
+  fi
+  echo "PASS: TestCommonRecJSON_001"
+} && TestCommonRecJSON_001
+
+TestCommonRecJSON_002() {
+  unset REC_SEVERITY
+  export REC_SEVERITY=0
+  if ! RecDefaultJSON "" testKey 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"DEFAULT","caller":"./test.sh","message":"","testKey":""}$'; then
+    echo "FAIL: TestCommonRecJSON_002"
+    return 1
+  fi
+  echo "PASS: TestCommonRecJSON_002"
+} && TestCommonRecJSON_002
+
+TestCommonRecJSON_003() {
+  unset REC_SEVERITY
+  export REC_SEVERITY=0
+  if ! RecDefaultJSON "" testKey testValue 2>&1 | grep -Eq '^{"timestamp":"[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.00:00","severity":"DEFAULT","caller":"./test.sh","message":"","testKey":"testValue"}$'; then
+    echo "FAIL: TestCommonRecJSON_003"
+    return 1
+  fi
+  echo "PASS: TestCommonRecJSON_003"
+} && TestCommonRecJSON_003
